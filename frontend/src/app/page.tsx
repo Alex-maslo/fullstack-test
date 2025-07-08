a754e65d8a852e8dcd0e47189bfd187b040b34fe
Alex-maslo <4439897@gmail.com>
2025-07-08 16:46:59 +0300

    feat(frontend): setup Next.js project with Tailwind CSS and DaisyUI

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <div className="navbar bg-neutral text-neutral-content">
+        <button className="btn btn-ghost text-xl">daisyUI</button>
+      </div>
+
+      <div className="navbar bg-base-300">
+        <button className="btn btn-ghost text-xl">daisyUI</button>
+      </div>
+
+      <div className="navbar bg-primary text-primary-content">
+        <button className="btn btn-ghost text-xl">daisyUI</button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
